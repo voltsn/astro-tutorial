8c66047edcc6071ac4100d8be293a86986f4c305
Arno Volts <voltsnntanisimine@gmail.com>
2025-05-11 22:00:51 +0200

    Add a greeting preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://spiffy-douhua-4040d7.netlify.app/"
+  site: "https://spiffy-douhua-4040d7.netlify.app/",
+  integrations: [preact()]
 });
