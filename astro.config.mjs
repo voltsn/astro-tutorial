e20fa994f79f9739a72b6013622f3b4c4cb5b97e
Arno Volts <voltsnntanisimine@gmail.com>
2025-05-11 21:26:59 +0200

    Add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://spiffy-douhua-4040d7.netlify.app/"
+});
